7ff03fd57cbbfe012a65a5c66531500e9082d056
zi Tortue <57108814+tortue733@users.noreply.github.com>
2024-07-04 22:29:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function checkBorpa() {
                     const [wallet, , amount] = lines[i].split(';');
                     if (wallet === address) {
                         found = true;
-                        const borpaAmount = parseFloat(amount) / (10 ** 6); // Adjust for Solana
+                        const borpaAmount = parseFloat(amount);
                         resultDiv.innerHTML = `Address: ${address} holds ${borpaAmount.toFixed(6)} Borpa.`;
                         break;
                     }
